defb6bcb15c841e6f1b8f2286e7587722c92aa78
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-10-02 21:47:44 +0600

    add toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from '../components/providers/theme-provider'
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ToasterProvider } from '@/components/providers/toaster-provider'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <ToasterProvider />
         </ThemeProvider>
       </body>
     </html>
